a33f4b358a43e8f024876109fbdc65c7584c69f3
mohammed <com4171@yahoo.com>
2025-03-24 12:36:42 +0100

    updating project

---
+++ apps/spreed/lib/Events/MessageParseEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2019 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Talk\Events;
+
+use OCA\Talk\Model\Message;
+use OCA\Talk\Room;
+
+class MessageParseEvent extends ARoomEvent {
+	public function __construct(
+		Room $room,
+		protected Message $message,
+	) {
+		parent::__construct($room);
+	}
+
+	public function getMessage(): Message {
+		return $this->message;
+	}
+}
