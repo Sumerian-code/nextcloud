425817495a9798986db6f2125e8d2a030b71ac6f
mohammed <com4171@yahoo.com>
2025-03-24 12:36:57 +0100

    updating project

---
+++ config/config.php
@@ -0,0 +1,26 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocke3a5iovq9',
+  'passwordsalt' => 'dTy4Avz3Eg1mc5HLO0I7OAFWjJFBhi',
+  'secret' => '/X2yOyH864gII2MHr606llfVA7HdnQhE+QSEBbDo8ieIqxvD',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost:8080',
+  ),
+  'datadirectory' => '/home/ffx/Desktop/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '31.0.2.1',
+  'overwrite.cli.url' => 'http://localhost:8080',
+  'dbname' => 'nextcloud_db',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'kali',
+  'installed' => true,
+  'app_install_overwrite' => 
+  array (
+    0 => 'keeporsweep',
+  ),
+);
